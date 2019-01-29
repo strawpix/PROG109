3b73b23ba68b285234b0e9da6a6f2cb63cdc69f3
strawpix <46378914+strawpix@users.noreply.github.com>
2019-01-28 19:24:56 -0800

    contact page

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="midtermstyle.css"/>
+		<title>Contact Me</title>
+		
+	</head>
+		<body>
+
+		<form action="name-of-your-thankyou-page.html" method="get"
+			name="contact">
+
+			...
+
+			</form>
+
+		</body>
+	</html>
